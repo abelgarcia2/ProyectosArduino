5c634f73fd18279a116b1cc3836824c6d293e36f
Abel Garcia <abelgg2000@gmail.com>
2021-05-03 12:21:31 +0200

    Subo ejercicio 1 de prácticas obligatorios

---
+++ Ejercicio1/Ejercicio1Modificado.ino
@@ -0,0 +1,18 @@
+int led = 11;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:   
+  digitalWrite(led, HIGH);
+  delay(200);       
+  digitalWrite(led, LOW);
+  delay(200);   
+
+  digitalWrite(led, HIGH);
+  delay(200);       
+  digitalWrite(led, LOW);
+  delay(1000);  
+}
